d3f8344f7e2d7f84fc12e997bc620e155c866965
prestonator <preston.b.jenkins-1@ou.edu>
2023-02-11 16:23:50 -0600

    blog cards better

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,28 +1,53 @@
 import { getPostData } from "@/src/api/fetchData/fetchPost";
-import BlogCard from "@/src/components/BlogCard";
+import { getMediaData } from "@/src/api/fetchData/fetchMedia";
+import BlogCard from "@/src/components/blogCardNew";
+import styles from "@/src/styles/pages/Blog.module.css";
+import urlBuilder from "@/src/utils/urlBuilder";
 
 export default async function Page() {
 	const { allPostData } = await getPostData();
-	// console.log(allPostData);
+	const [blogHeroQuery] = await getMediaData([9]);
+	const blogHeroUrl = urlBuilder(blogHeroQuery.attributes.url);
 	return (
 		<>
-			
-			<section className="sectionOne">
-				{allPostData.map((post) => (
-					<BlogCard
-						key={post.attributes.slug}
-						title={post.attributes.title}
-						author={post.attributes.author.data.attributes.name}
-						excerpt={post.attributes.excerpt}
-						date={post.attributes.datePublished}
-						image={post.attributes.image.data.attributes.url}
-						imageAlt={post.attributes.image.data.attributes.alternativeText}
-						slug={post.attributes.slug}
-						categories={post.attributes.categories.data
-							.map((item) => item.attributes.category)
-							.join(", ")}
-					/>
-				))}
+			<section
+				className={styles.sectionOne}
+				style={{
+					backgroundImage: `linear-gradient(var(--color-blue-overlay), var(--color-blue-overlay)), url(${blogHeroUrl})`,
+					backgroundSize: "cover",
+				}}
+			>
+				<div className={styles.row}>
+					<h1>
+						Blog Posts
+						<br />&<br />
+						Current News
+					</h1>
+				</div>
+			</section>
+			<section className={styles.sectionTwo}>
+				<div className={`${styles.row}`}>
+					{allPostData.map((post) => (
+						<div
+							className={`${styles.blogCardContainer}`}
+							key={post.attributes.slug}
+						>
+							<BlogCard
+								className={styles.blogCardContainer}
+								title={post.attributes.title}
+								author={post.attributes.author.data.attributes.name}
+								excerpt={post.attributes.excerpt}
+								date={post.attributes.datePublished}
+								image={post.attributes.image.data.attributes.url}
+								imageAlt={post.attributes.image.data.attributes.alternativeText}
+								slug={post.attributes.slug}
+								categories={post.attributes.categories.data
+									.map((item) => item.attributes.category)
+									.join(", ")}
+							/>
+						</div>
+					))}
+				</div>
 			</section>
 		</>
 	);
